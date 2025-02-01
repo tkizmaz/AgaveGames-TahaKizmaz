ee5d7d8ec63970614b35a38d3778bd5ca24cd04c
Taha Kızmaz <tahakizmaz@gmail.com>
2025-02-01 12:27:45 +0000

    feat(code): Add callback

--- Assets/Scripts/Abstracts/MovableObject.cs
+++ Assets/Scripts/Abstracts/MovableObject.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using System;
 
 public abstract class MovableObject : MonoBehaviour, IPoolable
 {
     public abstract void SpawnObject();
 
-    public virtual void MoveToCell(Cell targetCell)
+    public virtual void MoveToCell(Cell targetCell, Action OnComplete = null)
     {
         Vector3 newPosition = GridManager.Instance.GetWorldPositionFromGridPosition(targetCell.GridPosition);
-        transform.DOMove(newPosition, 1f).SetEase(Ease.OutBounce);
+        transform.DOMove(newPosition, 1f).SetEase(Ease.OutBounce).OnComplete(() =>
+        {
+            OnComplete?.Invoke();
+        });
     }
 
     public virtual void OnSpawnFromPool()
