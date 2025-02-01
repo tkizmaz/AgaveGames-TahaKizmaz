1082f7cd9bde102f67376d85a7e497e8702b5c6e
Taha Kızmaz <tahakizmaz@gmail.com>
2025-02-01 02:20:04 +0000

    refactor(logic): Use tile data

--- Assets/Scripts/Classes/Tile.cs
+++ Assets/Scripts/Classes/Tile.cs
@@ -2,13 +2,32 @@ using UnityEngine;
 
 public class Tile : MovableObject
 {
-    private TileColor tileColor;
-    public TileColor TileColor => tileColor; // Tile'ın rengini dışarıdan erişilebilir yap
+    [SerializeField]
+    private TileData tileData;
+    public TileData TileData => tileData; // Tile'ın verilerine dışarıdan erişilebilir yap
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake() 
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void SetTileData(TileData tileData)
+    {
+        this.tileData = tileData;
+        spriteRenderer.sprite = tileData.tileSprite;
+    }
 
     public override void SpawnObject()
     {
-        tileColor = (TileColor)UnityEngine.Random.Range(0, System.Enum.GetNames(typeof(TileColor)).Length);
-        gameObject.GetComponent<SpriteRenderer>().sprite = ColorManager.Instance.GetSprite(tileColor);
-        gameObject.GetComponent<SpriteRenderer>().sortingOrder = 1;
+        TileColor randomColor = (TileColor)UnityEngine.Random.Range(0, System.Enum.GetNames(typeof(TileColor)).Length);
+
+        TileData selectedData = TileDatabase.Instance.GetTileDataByColor(randomColor);
+
+        if (selectedData != null)
+        {
+            SetTileData(selectedData);
+        }
+        spriteRenderer.sortingOrder = 1;
     }
 }
