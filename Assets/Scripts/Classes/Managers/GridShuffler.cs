4d1089e99d9771ef031c201016754d10f9c5a3f0
Taha Kızmaz <tahakizmaz@gmail.com>
2025-02-01 12:27:14 +0000

    feat(logic): Add grid shuffler class

---
+++ Assets/Scripts/Classes/Managers/GridShuffler.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class GridShuffler
+{
+    private Cell[,] grid;
+    private int columnCount;
+    private int rowCount;
+
+    public GridShuffler(Cell[,] grid, int columns, int rows)
+    {
+        this.grid = grid;
+        this.columnCount = columns;
+        this.rowCount = rows;
+    }
+
+    public void ShuffleBoard()
+    {
+        List<Tile> allTiles = new List<Tile>();
+        for (int x = 0; x < columnCount; x++)
+        {
+            for (int y = 0; y < rowCount; y++)
+            {
+                if (grid[x, y].IsOccupied)
+                {
+                    allTiles.Add(grid[x, y].CurrentTile);
+                }
+            }
+        }
+
+        int n = allTiles.Count;
+        for (int i = n - 1; i > 0; i--)
+        {
+            int randomIndex = Random.Range(0, i + 1);
+            (allTiles[i], allTiles[randomIndex]) = (allTiles[randomIndex], allTiles[i]);
+        }
+
+        int index = 0;
+        for (int x = 0; x < columnCount; x++)
+        {
+            for (int y = 0; y < rowCount; y++)
+            {
+                if (grid[x, y].IsOccupied)
+                {
+                    Tile tile = allTiles[index++];
+                    tile.MoveToCell(grid[x, y]);
+                    grid[x, y].SetTile(tile);
+                }
+            }
+        }
+    }
+}
