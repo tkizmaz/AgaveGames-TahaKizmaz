369bb564c7ea1e67c30d09968755d17f8e1eb3b7
Taha Kızmaz <tahakizmaz@gmail.com>
2025-02-01 12:26:19 +0000

    feat(code): Add new event

--- Assets/Scripts/Classes/Events/GameEvents.cs
+++ Assets/Scripts/Classes/Events/GameEvents.cs
@@ -9,4 +9,5 @@ public static class GameEvents
     public static Action<Tile> OnTileDestroyed; 
     public static Action OnGameWon;
     public static Action OnGameLost;
+    public static Action<GameState> OnGameStateChanged;
 }
