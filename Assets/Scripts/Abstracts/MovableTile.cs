70a7b0e27e4a847a64ddbcd575553b94e38c6551
Taha Kızmaz <tahakizmaz@gmail.com>
2025-02-02 23:36:42 +0000

    refactor(code): Add movetime

--- Assets/Scripts/Abstracts/MovableTile.cs
+++ Assets/Scripts/Abstracts/MovableTile.cs
@@ -7,16 +7,12 @@ public abstract class MovableTile : Tile
     public virtual void MoveToCell(Cell targetCell, Action OnComplete = null)
     {
         if (targetCell == null) return;
-
+    
         Vector3 newPosition = targetCell.transform.position;
-        transform.DOMove(newPosition, 1f).SetEase(Ease.OutBounce).OnComplete(() =>
+        float moveTime = Mathf.Clamp(Vector3.Distance(transform.position, newPosition) * 0.5f, 0.2f, 1.0f);
+        transform.DOMove(newPosition, moveTime).SetEase(Ease.OutBounce).OnComplete(() =>
         {
             OnComplete?.Invoke();
         });
-    }
-
-    public override void OnReturnToPool()
-    {
-        base.OnReturnToPool();
     }
 }
