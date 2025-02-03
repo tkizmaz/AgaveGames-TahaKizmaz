45f922001e322b103b8d52e7cb1997ecd3c54a57
Taha Kızmaz <tahakizmaz@gmail.com>
2025-02-03 03:34:40 +0000

    refactor(code): Remove unused events

--- Assets/Scripts/Classes/Events/GameEvents.cs
+++ Assets/Scripts/Classes/Events/GameEvents.cs
@@ -7,7 +7,5 @@ public static class GameEvents
     public static Action<TileData> OnGoalTileChanged; 
     public static Action<int> OnGoalTileCountChanged; 
     public static Action<Tile> OnTileDestroyed; 
-    public static Action OnGameWon;
-    public static Action OnGameLost;
     public static Action<GameState> OnGameStateChanged;
 }
